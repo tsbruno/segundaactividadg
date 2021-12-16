a2e248bd8718f763076d4c005f26876227fb4d71
bruno torres <tsoriabruno@gmail.com>
2021-12-15 22:25:46 -0600

    segundaaactividadgeneration

---
+++ actividadDos/segundaActividad.js
@@ -0,0 +1,15 @@
+
+
+let nombre = "Bruno"; 
+    console.log("Hola mi nombre es: " + nombre); 
+
+let numeroFavorito = "7"; 
+    console.log("Mi numero favorito es el: " + numeroFavorito); 
+
+let respuesta = "no";
+    console.log(" Tengo mascotas?..." + respuesta);  
+
+
+
+
+
